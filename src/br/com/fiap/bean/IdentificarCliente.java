917fc0c857dfff4220efc938837caa92b543cc98
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-06 19:56:57 -0300

    exclui a classe TipoSeguro porque agora fica direto na main, criei a base do código do IdentificarCliente e comecei a fazer o case 2 do menu

--- src/br/com/fiap/bean/IdentificarCliente.java
+++ src/br/com/fiap/bean/IdentificarCliente.java
@@ -1,5 +1,34 @@
 package br.com.fiap.bean;
 
-public class IdentificarCliente {
+import javax.swing.JOptionPane;
 
+public class IdentificarCliente {
+	private String cpf;
+
+	//Construtor vazio
+	public IdentificarCliente() {
+
+	}
+
+	//Getters e setters
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	//Método
+	//A fazer: tem que fazer a verificação se o cpf digitado está no sistema (tem que retornar sempre true)
+	public void encontrarCliente() {
+		if (cpf == true) {
+			JOptionPane.showMessageDialog(null, "Olá, seja bem vindo");
+		} else {
+			JOptionPane.showMessageDialog(null, "CPF não encontrado. Faça seu cadastro e contrate um seguro para sua bike!");
+		}
+	}
+	
+	
+	
 }
--- src/br/com/fiap/main/UsaTechnobike.java
+++ src/br/com/fiap/main/UsaTechnobike.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 
 import br.com.fiap.bean.RegistroSeguro;
 import br.com.fiap.bean.Feedback;
+import br.com.fiap.bean.IdentificarCliente;
 
 public class UsaTechnobike {
 
@@ -12,9 +13,12 @@ public class UsaTechnobike {
 		int mostrarStatus;
 		String aux;
 		String escolha = "sim";
+		String cpf;
+		Boolean aprovado = false, reprovado = false, emAnalise = true, faltandoDoces = false;
 		
-		RegistroSeguro tpseg = new RegistroSeguro();
-		Feedback fb = new Feedback();
+		IdentificarCliente identCli;
+		//RegistroSeguro tpseg = new RegistroSeguro();
+		//Feedback fb = new Feedback();
 		
 		while (escolha.equalsIgnoreCase("sim")) {	
 			try {
@@ -34,7 +38,7 @@ public class UsaTechnobike {
 
 			//Tipos de seguro
 			case 1:
-				JOptionPane.showMessageDialog(null, "Essas são as opções de seguro disponibilizadas pela nossa empresa"
+				JOptionPane.showMessageDialog(null, "Essas são as opções de seguro disponibilizadas pela nossa empresa: "
 												+ "\n1- Para ciclistas que pedalam na rua"
 												+ "\n2- Para ciclistas de maratona"
 												+ "\n3- Para ciclistas que pedalam em montanhas"
@@ -47,21 +51,21 @@ public class UsaTechnobike {
 				
 			//Iniciar processo de vistoria	
 			case 2:
-				JOptionPane.showMessageDialog(null, "Informe seus dados");
-				ftBike.confirmarArquivo();
-				vdBike.confirmarArquivo();
-				JOptionPane.showMessageDialog(null, "Escolha o tipo de seguro");
-				tpseg.selecionaSeguro();
-				aux = JOptionPane.showInputDialog("Os seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu e-mail ou aqui pelo site." 
-												+ "Deseja conferir o status da análise da vistoria? \n1 - Sim \n2 - Não");
-				mostrarStatus = Integer.parseInt(aux);
-				if (mostrarStatus == 1) {
-					
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "Ok. Acompanhe no seu email ou nessa tela o atual status da sua vistoria para saber as informações de como prosseguir.");
-				}
-				break;
+				cpf = JOptionPane.showInputDialog("Informe seu CPF: ");
+				
+				identCli = new IdentificarCliente();
+				
+				identCli.encontrarCliente();
+				//JOptionPane.showMessageDialog(null, "Escolha o tipo de seguro");
+				//tpseg.selecionaSeguro();
+				//aux = JOptionPane.showInputDialog("Os seus dados foram enviados para vistoria. Você pode acompanhar o atual status da análise pelo seu e-mail ou aqui pelo site." 
+				//								+ "Deseja conferir o status da análise da vistoria? \n1 - Sim \n2 - Não");
+				//mostrarStatus = Integer.parseInt(aux);
+				//if (mostrarStatus == 1) {	
+				//} else {
+				//	JOptionPane.showMessageDialog(null, "Ok. Acompanhe no seu email ou nessa tela o atual status da sua vistoria para saber as informações de como prosseguir.");
+				//}
+				//break;
 		
 				
 			//Conferir status da vistoria
