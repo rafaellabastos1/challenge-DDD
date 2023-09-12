ae367f2af2a89a8cea1314168be01f783c059d15
rafaellabastos1 <rm552425@fiap.com.br>
2023-09-12 18:20:13 -0300

    parte do registro seguro finalizada com tratamento de bugs

--- src/br/com/fiap/main/UsaTechnobike.java
+++ src/br/com/fiap/main/UsaTechnobike.java
@@ -65,17 +65,19 @@ public class UsaTechnobike {
 				identCli.encontrarCliente();
 				
 				//Registrar tipo de seguro
+				regSeg = new RegistroSeguro();
+				
 				aux = JOptionPane.showInputDialog("Selecione um tipo de seguro: "
 												+ "\n1- Para ciclistas que pedalam na rua"
-												+ "\n2- Para ciclistas de maratona"
+												+ "\n2- Para ciclistas de maratona" 
 												+ "\n3- Para ciclistas que pedalam em montanhas"
 												+ "\n4- Para ciclistas que pedalam em pedras e rochas"
 												+ "\n5- Para ciclistas que pedalam em terra e mato"
-												+ "\n6- Para ciclistas por hobbie"
-												+ "\n7- Para ciclistas que viajam com a bike");
+												+ "\n6- Para ciclistas por hobbie" 
+												+ "\n7- Para ciclistas que viajam com a bike");				
 				opcSeguro = Integer.parseInt(aux);
 				
-				regSeg = new RegistroSeguro();
+				regSeg.setOpcSeguro(opcSeguro);
 				regSeg.selecionaSeguro();
 				
 				//Receber as mídias da vistoria
